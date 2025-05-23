6451ca6dfcdaf5f227cc7443b36bf2f5f26c04bf
JuliaMuk <mukhina_ur@mail.ru>
2025-05-23 15:06:36 +0500

    for and while

--- js/script.js
+++ js/script.js
@@ -24,42 +24,78 @@
 
 {
     /*Дано 4-значное число, найти все его цифры */
-    let a = 14 % 5; //остаток от деления
-    let b = Math.floor(14 % 5); //целочисленное деление
-    console.log(a, b);
+    // let a = 14 % 5; //остаток от деления
+    // let b = Math.floor(14 % 5); //целочисленное деление
+    // console.log(a, b);
 
 
-    let number,d1,d2,d3,d4;
-    number = parseInt(prompt("Введите 4-значное число"));
-    d1 = Math.floor(number/1) % 10; 
-    d2 = Math.floor(number/10) % 10;
-    d3 = Math.floor(number/100) % 10;
-    d4 = Math.floor(number/1000) % 10;
-    console.log(d1,d2,d3,d4);
+    // let number,d1,d2,d3,d4;
+    // number = parseInt(prompt("Введите 4-значное число"));
+    // d1 = Math.floor(number/1) % 10; 
+    // d2 = Math.floor(number/10) % 10;
+    // d3 = Math.floor(number/100) % 10;
+    // d4 = Math.floor(number/1000) % 10;
+    // console.log(d1,d2,d3,d4);
 }
 
 {
-    /*Найти максимум из двух чисел */
-    let x, y, max;
-    x = parseFloat(prompt("Введите первое число"));
-    y = parseFloat(prompt("Введите второе число"));
-    if(x > y){
-        max = x;
-    }
-    else {
-        max = y;
-    }
-    alert("max = " + max);
+    /*Найти максимум из трех чисел */
+    // let x, y, z, max;
+    // x = parseFloat(prompt("Введите первое число"));
+    // y = parseFloat(prompt("Введите второе число"));
+    // z = parseFloat(prompt("Введите третье число"));
+    // if(x >= y && x >= z){
+    //     max = x;
+    // }
+    // else if(y >= x && y >= z) {
+    //     max = y;
+    // }
+    // else {
+    //     max = z;
+    // }
+    // alert("max = " + max);
 }
 
 {
-    /*Найти максимум из двух чисел - 2 способ*/
-    let x, y, max;
-    x = parseFloat(prompt("Введите первое число"));
-    y = parseFloat(prompt("Введите второе число"));
-    max = x;
-    if(y > max){
-        max = y;
+    /*Найти максимум из трех чисел - 2 способ*/
+    // let x, y, z, max;
+    // x = parseFloat(prompt("Введите первое число"));
+    // y = parseFloat(prompt("Введите второе число"));
+    // z = parseFloat(prompt("Введите третье число"));
+    // max = x;
+    // if(y > max){
+    //     max = y;
+    // }
+    // if(z > max){
+    //     max = z;
+    // }
+    // alert("max = " + max);
+}
+
+{
+    /*Даны n натуральных чисел. Найти максимум среди них. */
+    // let n, a, max;
+    // n = parseInt(prompt("Введите кол-во числел"));
+    // max = parseInt(prompt("Введите число"));
+    // for(let i = 1; i < n; i++){
+    //     a = parseInt(prompt("Введите число"));
+    //     if(a > max){
+    //         max = a;
+    //     }
+    // }
+    // alert("max = " + max);
+}
+
+{
+    /*Дана последовательность натуральных чисел, которая заканчивается вводом числа 0. Найти максимум среди этой последовательности. */
+    let a, max;
+    a = parseInt(prompt("Введите число"));
+    max = a;
+    while(a != 0){
+        a = parseInt(prompt("Введите число"));
+        if(a > max){
+            max = a;
+        }
     }
     alert("max = " + max);
 }
