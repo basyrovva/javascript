762f264817d7600ab5e8a6992d403ce119b7cbb7
JuliaMuk <mukhina_ur@mail.ru>
2025-05-23 13:48:18 +0500

    simple prog

--- js/script.js
+++ js/script.js
@@ -5,3 +5,37 @@
     mul = a * b;
     console.log(mul);
 }
+
+{
+    // let  photosInFolder,photosOnPage, numberOfPage;
+    // photosInFolder = parseInt(prompt("Введите кол-во фото в папке", 10));
+    // photosOnPage = parseInt(prompt("Введите кол-во фото на одной странице", 10));
+
+    // numberOfPage = Math.ceil(photosInFolder / photosOnPage);
+
+    // alert("кол-во страниц = " + numberOfPage);
+}
+
+{
+    let a = 1,b;
+    b = ++a;
+    console.log(a, b);
+}
+
+{
+    /*Дано 4-значное число, найти все его цифры */
+    let a = 14 % 5; //остаток от деления
+    let b = Math.floor(14 % 5); //целочисленное деление
+    console.log(a, b);
+
+
+    let number,d1,d2,d3,d4;
+    number = parseInt(prompt("Введите 4-значное число"));
+    d1 = Math.floor(number/1) % 10; 
+    d2 = Math.floor(number/10) % 10;
+    d3 = Math.floor(number/100) % 10;
+    d4 = Math.floor(number/1000) % 10;
+    console.log(d1,d2,d3,d4);
+}
+
+
