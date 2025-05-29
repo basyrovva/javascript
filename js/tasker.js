fd6ccb4aff9fbcc45a801c043d6a2292169074bb
basyrovva <basyrovg1@mail.ru>
2025-05-29 12:58:02 +0500

    create tasker

--- js/tasker.js
+++ js/tasker.js
@@ -1,4 +1,4 @@
-const container = document.querySelector(".container");
+let container = document.querySelector(".container");
 const btnCreat = document.getElementById("btn-create-task");
 const newTask = document.getElementById("new-task");
 
@@ -8,11 +8,55 @@ let tasks = document.querySelectorAll(".task");
 //     return item + "!";
 // });
 
-
-
+const btn1 = document.getElementById("btn1");
+const btn2 = document.getElementById("btn2");
+const btn3 = document.getElementById("btn3");
+const immediate = document.querySelectorAll(".immeadiate");
+function compare(a, b) { //функция сравнения для сортировки
+    //если первое значение больше второго
+    if (a.querySelector("span").innerHTML > b.querySelector("span").innerHTML) return 1;
+    //если равны
+    if (a.querySelector("span").innerHTML == b.querySelector("span").innerHTML) return 0;
+    //первое значеие меньше второго
+    if (a.querySelector("span").innerHTML < b.querySelector("span").innerHTML) return -1;
+}
+function compareReverse(a, b) { //функция сравнения для сортировки
+    //если первое значение больше второго
+    if (a.querySelector("span").innerHTML > b.querySelector("span").innerHTML) return -1;
+    //если равны
+    if (a.querySelector("span").innerHTML == b.querySelector("span").innerHTML) return 0;
+    //первое значеие меньше второго
+    if (a.querySelector("span").innerHTML < b.querySelector("span").innerHTML) return 1;
+}
+function filter(a, b) { //функция сравнения для сортировки
+    //если первое значение больше второго
+    if (a.querySelector("").innerHTML > b.querySelector("span").innerHTML) return -1;
+    //если равны
+    if (a.querySelector("span").innerHTML == b.querySelector("span").innerHTML) return 0;
+    //первое значеие меньше второго
+    if (a.querySelector("span").innerHTML < b.querySelector("span").innerHTML) return 1;
+}
+btn1.addEventListener("click", function () {
+    let newTasks = [...tasks].sort(compare);
+    for (let i in newTasks) { //перебор всех элементов в массиве
+        container.append(newTasks[i]) //добавление новых элементов в контейнер
+    }
+})
+btn2.addEventListener("click", function () {
+    let newTasks = [...tasks].sort(compareReverse);
+    for (let i in newTasks) { //перебор всех элементов в массиве
+        container.append(newTasks[i]) //добавление новых элементов в контейнер
+    }
+})
+btn3.addEventListener("click", function(){
+    let newTasks = [...tasks].filter(immediate);
+    for (let i in newTasks) { //перебор всех элементов в массиве
+        container.append(newTasks[i]) //добавление новых элементов в контейнер
+    }
+})
 
 //добавление новой задачи
-btnCreat.addEventListener("click", function(event){
+btnCreat.addEventListener("click", function (event) {
     event.preventDefault();//отменяем повдение по умолчанию - форма не обновляет страницу
     let text = newTask.value;
     const task = document.createElement("div");
@@ -25,7 +69,7 @@ btnCreat.addEventListener("click", function(event){
 
 //удаление всех задач
 const btnRemove = document.getElementById("btn-remove");
-btnRemove.addEventListener("click", function(){
+btnRemove.addEventListener("click", function () {
     container.innerHTML = "";
     tasks = document.querySelectorAll(".task");
     console.log(tasks);
